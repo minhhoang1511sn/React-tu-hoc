0b62f703999d7eca6524af25e5c74ab25b66a40f
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2025-06-22 16:37:40 +0700

    bai 33

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,9 @@
+const TodoData = ()=>{
+    return(
+<div className="todo-data">
+            <div>LEaring react</div>
+          </div>
+    );
+}
+
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,11 @@
+const TodoNew = ()=>{
+    return(
+<div className="todo-new">
+
+          <input className='input-todo' type="text" placeholder='Enter your task'/>
+          <button className='btnAdd'>Add</button>
+          </div>
+    );
+}
+
+export default TodoNew;
